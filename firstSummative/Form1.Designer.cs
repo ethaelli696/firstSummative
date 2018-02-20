a0ac0d7680addda9cdf0bcc22627f8557ac9fecb
Ethan Elliott <ethaelli696@ed.amdsb.ca>
2018-02-20 13:35:57 -0500

    drew constellation

--- firstSummative/Form1.Designer.cs
+++ firstSummative/Form1.Designer.cs
@@ -28,12 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.happyHannukah = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // happyHannukah
+            // 
+            this.happyHannukah.AutoSize = true;
+            this.happyHannukah.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.happyHannukah.Location = new System.Drawing.Point(256, 263);
+            this.happyHannukah.Name = "happyHannukah";
+            this.happyHannukah.Size = new System.Drawing.Size(97, 25);
+            this.happyHannukah.TabIndex = 0;
+            this.happyHannukah.Text = "Hercules";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(740, 583);
+            this.Controls.Add(this.happyHannukah);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label happyHannukah;
     }
 }
 
--- firstSummative/Form1.cs
+++ firstSummative/Form1.cs
@@ -7,7 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
+using System.Media;
 
+//Ethan Elliott
 namespace firstSummative
 {
     public partial class Form1 : Form
@@ -16,5 +19,83 @@ namespace firstSummative
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            //get rid of happy hannukah
+            happyHannukah.Visible = false;
+
+            //create sound players
+            SoundPlayer boop = new SoundPlayer(Properties.Resources.Boop);
+            SoundPlayer shimmer = new SoundPlayer(Properties.Resources.Star);
+
+            //create graphics objects
+            Graphics g = this.CreateGraphics();
+            Font bigFont = new Font("Helvetica", 24, FontStyle.Bold);
+            SolidBrush drawBrush = new SolidBrush(Color.White);
+            Pen constellationPen = new Pen(Color.White, 8);
+            Pen redPen = new Pen(Color.Red, 8);
+            Pen greenPen = new Pen(Color.Green, 8);
+          
+            //create constellation
+            BackColor = (Color.Black);
+            Refresh();
+            shimmer.Play();
+            Thread.Sleep(1000);
+            g.DrawLine(redPen, 197, 168, 105, 289);
+            boop.Play();
+
+            Thread.Sleep(500);
+            g.DrawLine(constellationPen, 106, 300, 203, 327);
+            boop.Play();
+            Thread.Sleep(500);
+            g.DrawLine(redPen, 220, 330, 236, 341);
+            boop.Play();
+            Thread.Sleep(500);
+            g.DrawLine(greenPen, 250, 339, 352, 323);
+            boop.Play();
+            Thread.Sleep(500);
+            g.DrawLine(constellationPen, 366, 315, 445, 198);
+            boop.Play();
+            Thread.Sleep(500);
+            g.DrawLine(redPen, 453, 199, 667, 264);
+            boop.Play();
+            Thread.Sleep(500);
+            g.DrawLine(greenPen, 19, 431, 44, 440);
+            boop.Play();
+            Thread.Sleep(500);
+            g.DrawLine(constellationPen, 57, 439, 194, 539);
+            boop.Play();
+            Thread.Sleep(500);
+            g.DrawLine(redPen, 207, 545, 276, 460);
+            boop.Play();
+            Thread.Sleep(500);
+            g.DrawLine(greenPen, 276, 446, 346, 453);
+            boop.Play();
+            Thread.Sleep(500);
+            g.DrawLine(constellationPen, 273, 446, 246, 350);
+            boop.Play();
+            Thread.Sleep(500);
+            g.DrawLine(redPen, 352, 441, 361, 328);
+            boop.Play();
+            Thread.Sleep(500);
+            g.DrawLine(greenPen, 359, 445, 369, 568);
+            boop.Play();
+
+            //draw paragraph and text
+
+            g.DrawString("Hercules", bigFont, drawBrush, 475, 30);
+
+
+
+
+        }
+
+
     }
 }
